80bf4004b59cf3cc5a192c88871d8b1fe966b7a9
Vince Suriani <chenzorama@hotmail.com>
2019-07-16 07:38:04 -0400

    Video Stream Working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {hot} from "react-hot-loader";
 import "./App.scss";
 import MicCircle from "./components/MicCircle";
 import AudioAnalyser from "./components/AudioAnalyser"
+import VideoOutput from "./components/VideoOutput"
+
 
 class App extends Component{
   constructor(props) {
@@ -11,12 +13,22 @@ class App extends Component{
     this.state = {
       isToggleOn: false,
       Volume: 0,
-      audio: null
+      audio: null,
+      video: null
     };
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleVideoClick = this.handleVideoClick.bind(this);
   }
 
+
+  async getVideo() {
+    const video = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: true
+    });
+    this.setState({ video });
+  }
 
   async getMicrophone() {
     const audio = await navigator.mediaDevices.getUserMedia({
@@ -31,6 +43,11 @@ class App extends Component{
     this.setState({ audio: null });
   }
 
+  stopVideo() {
+    this.state.video.getTracks().forEach(track => track.stop());
+    this.setState({ video: null });
+  }
+
   handleClick() {
     this.setState(state => ({
       isToggleOn: !state.isToggleOn
@@ -43,18 +60,33 @@ class App extends Component{
     } 
   }
 
-  componentDidMount() {
-    
+
+  handleVideoClick() {
+    if (this.state.video) {
+      this.stopVideo();
+    } else {
+      this.getVideo();
+    } 
   }
 
   render(){
     return(
       <div className="App">
-        <button onClick={this.handleClick}>
-          {this.state.isToggleOn ? 'Mic On' : 'Mic Off'}
-        </button>
+        <div className="controls">
+          <button onClick={this.handleClick}>
+            {this.state.isToggleOn ? 'Mic On' : 'Mic Off'}
+          </button>
+          <button onClick={this.handleVideoClick}>
+            {this.state.video ? 'Vid On' : 'Vid Off'}
+          </button>
+        </div>
+        <div>
         {this.state.audio ? <MicCircle audio={this.state.audio} /> : ''}
         {/* {this.state.audio ? <AudioAnalyser audio={this.state.audio} /> : ''} */}
+        </div>
+        
+        {this.state.video ? <VideoOutput video={this.state.video} /> : ''}
+        
       </div>
     );
   }
